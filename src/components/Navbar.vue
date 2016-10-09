adbb48e0df464c620617fcaefd347f5b8fe8b96a
bc_rikko <s.shin1106@gmail.com>
2016-10-09 15:18:22 +0900

    :name_badge: ProfileNavbarの追加

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,64 @@
 <template>
-  <nav></nav>
+  <div class="navbar">
+    <div class="app-container">
+      <div class="mini-profile">
+      </div><!--
+   --><div class="profile-navbar">
+        <ul>
+          <li class="profile-navbar--active"><a href="/"><span>経歴</span></a></li>
+          <li><a href="/"><span>作ったもの</span></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-  nav {
-    height: 60px;
-    background-color: #fff;
-  }
+<style scoped>
+.navbar {
+  height: 50px;
+  background-color: #fff;
+}
+
+.mini-profile {
+  display: inline-block;
+  width: 30%;
+  min-width: 200px;
+  box-sizing: border-box;
+}
+.profile-navbar {
+  display: inline-block;
+  width: 70%;
+  min-width: 500px;
+  box-sizing: border-box;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+ul > li {
+  display: inline-block;
+  width: 85px;
+  padding: 0 15px;
+
+  border-bottom: 5px solid transparent;
+  transition: all .15s ease-in-out;
+}
+li > a {
+  display: block;
+  text-decoration: none;
+  height: 50px;
+  text-align: center;
+  line-height: 40px;
+  color: #66757f;
+  font-size: 1.2em;
+}
+li:hover,
+li > a:hover,
+.profile-navbar--active {
+  height: 46px;
+  color: #7708a3;
+  border-bottom: 5px solid #7708a3;
+}
 </style>
