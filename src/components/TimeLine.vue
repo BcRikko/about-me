310f2288d79c16fb251c8692e0a1e6cfc37405b6
bc_rikko <s.shin1106@gmail.com>
2016-10-09 15:30:34 +0900

    :end: TimeLineの終端を追加

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -3,6 +3,9 @@
     <ol>
       <card v-for="timeline in timelines" :profile="profile" :timeline="timeline"></card>
     </ol>
+    <div class="time-line__end">
+      <i class="fa fa-twitter"></i>
+    </div>
   </div>
 </template>
 
@@ -18,11 +21,25 @@
   min-width: 500px;
 
   background-color: #fff;
+
+  margin-bottom: 50px;
 }
 
 ol {
   padding: 0;
   margin: 0;
+}
+
+.time-line__end {
+  height: 60px;
+  text-align: center;
+  line-height: 60px;
+
+  border: 1px solid #e1e8ed;
+}
+.time-line__end > i {
+  font-size: 24px;
+  color: #66757f;
 }
 </style>
 
